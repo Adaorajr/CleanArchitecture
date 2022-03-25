8b21f1712eebea1601ff2e2a78dd941e9d0f8c83
Adão Ribeiro <sarinjr1024@hotmail.com>
2022-03-25 14:10:04 -0400

    Remoção Filter

--- src/CleanArchitecture.Api/Filters/Interface/IExceptionFilter.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.Filters;
-
-namespace CleanArchitecture.Api.Filters.Interface
-{
-    public interface IExceptionFilter : IFilterMetadata
-    {
-        void OnException(ExceptionContext context);
-    }
-}
